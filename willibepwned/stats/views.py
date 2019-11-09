b9b51376733b02f75afe15e7f99e9484627ebc60
Kahlia <klepple@uwo.ca>
2019-11-09 18:18:23 -0500

    Switch back to current user infor

--- willibepwned/stats/views.py
+++ willibepwned/stats/views.py
@@ -33,16 +33,16 @@ def giveimages(request):
 
 def send_email(request):
     # Option 1: Get random user object from database
-    user_obj = smuck.objects.all()
-    random_item = random.choice(user_obj)
-    username = random_item.username
-    password = random_item.password
-    to_email = random_item.email
+    # user_obj = smuck.objects.all()
+    # random_item = random.choice(user_obj)
+    # username = random_item.username
+    # password = random_item.password
+    # to_email = random_item.email
 
     # Option 2: Use current user object
-    # username = request.POST.get('username', '')
-    # password = request.POST.get('password', '')
-    # to_email = request.POST.get('email', '')
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    to_email = request.POST.get('email', '')
 
     subject = "YOU'VE BEEN HAXXED N00B!!"
     message = "Hello, we are elite l33t hacker team. You system has been comprimised. \
