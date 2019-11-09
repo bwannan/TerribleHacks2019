77ebe94242a79146545687e35dca872f6d799032
Kahlia <klepple@uwo.ca>
2019-11-09 15:02:14 -0500

    fix merge conflict

--- willibepwned/willibepwned/urls.py
+++ willibepwned/willibepwned/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+<<<<<<< Updated upstream
 from .views import Index
 
 urlpatterns = [
     path('', include('main.urls')),
+    path('sendmail/', include('sendmail.urls')),
     path('admin/', admin.site.urls),
     path('', Index.as_view(), name="Index"),
 ]
