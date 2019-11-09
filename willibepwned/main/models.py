0f6b59d97dac64065467e6fd52a995234409965d
paulyhong2000 <paulyhong2000@gmail.com>
2019-11-09 14:17:56 -0500

    [user]model and migration

--- willibepwned/main/models.py
+++ willibepwned/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class smuck(models.Model):
+    email = models.CharField(max_length = 100)
+    username = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
