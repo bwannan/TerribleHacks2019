1150442f93967d6dab98f57313005f8e394bf471
Nuliflyer <tyler@hyperweb.ca>
2019-11-09 15:19:50 -0500

    moved header into base.html

--- willibepwned/main/views.py
+++ willibepwned/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+
+class captcha(TemplateView):
+    template_name='captcha.html'
